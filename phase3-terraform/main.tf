81d7f20251ab4130d0ffc34228a40d51170cbada
najimoon <sknajimoon143@gmail.com>
2026-06-23 18:06:04 +0530

    Change EC2 instance type to t3 micro

--- phase3-terraform/main.tf
+++ phase3-terraform/main.tf
@@ -224,7 +224,7 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 
 resource "aws_instance" "app_server" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public_1.id
   vpc_security_group_ids = [aws_security_group.app_sg.id]
   key_name               = var.key_name
